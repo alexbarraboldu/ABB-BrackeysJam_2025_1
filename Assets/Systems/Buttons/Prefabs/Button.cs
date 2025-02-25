2a6edbefd9da47b78a398e4cdbb3188153e1dd39
alexbarra <alexbarra00@gmail.com>
2025-02-26 00:21:23 +0100

    Added Button Sound

--- Assets/Systems/Buttons/Prefabs/Button.cs
+++ Assets/Systems/Buttons/Prefabs/Button.cs
@@ -1,9 +1,15 @@
+using System.Collections;
+
+using TMPro;
+
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Button : MonoBehaviour, IOnNotify
 {
 	private Animator _animator;
+
+	[SerializeField] private AudioClip _buttonSound = null;
 
 	public UnityAction IsPressed;
 
@@ -12,11 +18,26 @@ public class Button : MonoBehaviour, IOnNotify
 		_animator = GetComponent<Animator>();
 	}
 
+
+	private bool _isPressed = false;
 	public void OnNotify()
 	{
-		if (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1f) return;
+		if (!_isPressed)
+		{
+			_isPressed = true;
+			IsPressed.Invoke();
 
-		IsPressed.Invoke();
-		_animator.SetTrigger("Press");
+			SoundEffectManager.Instance.PlaySoundClip(_buttonSound, transform, 1f);
+
+			_animator.SetTrigger("Press");
+			AnimatorStateInfo stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+			StartCoroutine(ResetPress(stateInfo.length));
+		}
+	}
+
+	private IEnumerator ResetPress(float value)
+	{
+		yield return new WaitForSeconds(value);
+		_isPressed = false;
 	}
 }
